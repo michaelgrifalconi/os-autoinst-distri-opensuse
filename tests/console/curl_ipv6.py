51fc721aec1f8f8e328c6478ce35cace17af4326
Michael Grifalconi <mgrifalconi@suse.com>
2023-04-05 10:59:42 +0200

    wip

--- tests/console/curl_ipv6.py
+++ tests/console/curl_ipv6.py
@@ -1,5 +1,6 @@
 from testapi import *
 
 def run(self):
+    assert_script_run('echo "Hey, I am running on Python!"')
     assert_script_run('curl www3.zq1.de/test.txt')
     assert_script_run('rpm -q curl libcurl4')
