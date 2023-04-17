670f2d7ebafc56650056528eb187bdd42a9cb1d8
Michael Grifalconi <mgrifalconi@suse.com>
2023-04-17 17:31:07 +0200

    increase zypper timeout

--- tests/console/zypper_in.pm
+++ tests/console/zypper_in.pm
@@ -34,7 +34,7 @@ use serial_terminal 'select_serial_terminal';
 # fi
 
 sub run {
-    my $timeout = 420;
+    my $timeout = 600;
     select_serial_terminal;
     script_run("zypper lr -d | tee /dev/$serialdev", timeout => $timeout);
     my $pkgname = get_var('PACKAGETOINSTALL');
