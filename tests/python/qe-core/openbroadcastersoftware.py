59f56dc8f884c637cf71f9b30f6cf4bcbba03190
Michael Grifalconi <mgrifalconi@suse.com>
2023-05-25 14:59:41 +0200

    wip

--- tests/python/qe-core/openbroadcastersoftware.py
+++ tests/python/qe-core/openbroadcastersoftware.py
@@ -4,9 +4,10 @@ from testapi import *
 def run(self):
 
     ensure_installed("flatpak")
-
+    select_serial_terminal;
     assert_script_run('flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo')
     assert_script_run('flatpak install -y com.obsproject.Studio', timeout=300)
+    select_console 'x11';
     x11_start_program('obs', match_timeout=60)
     # # sometimes send_key "alt-f4" doesn't work reliable, so repeat it and exit
     # send_key_until_needlematch 'generic-desktop', "alt-f4", 6, 5;
