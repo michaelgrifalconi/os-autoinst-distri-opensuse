115117b21fcd92a54bb0d33aeaa33b7df0f0f575
Michael Grifalconi <mgrifalconi@suse.com>
2023-05-25 10:55:42 +0200

    wip

---
+++ tests/python/qe-core/openbroadcastersoftware.py
@@ -0,0 +1,25 @@
+from testapi import *
+
+
+def run(self):
+
+    ensure_installed("flatpak");
+
+    # assert_script_run('flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo');
+    # assert_script_run('flatpak install -y com.obsproject.Studio', timeout => 300);
+    # x11_start_program('gimp', match_timeout => 60);
+    # # sometimes send_key "alt-f4" doesn't work reliable, so repeat it and exit
+    # send_key_until_needlematch 'generic-desktop', "alt-f4", 6, 5;
+
+
+def switch_to_root_console():
+    send_key('ctrl-alt-f3')
+
+
+def post_fail_hook(self):
+    switch_to_root_console()
+    assert_script_run('openqa-cli api experimental/search q=shutdown.pm')
+
+
+def test_flags(self):
+    return {'fatal': 1}
