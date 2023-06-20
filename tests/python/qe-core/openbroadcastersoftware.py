0d6d959653b25965665c53c868a11c46e12f10e4
Michael Grifalconi <mgrifalconi@suse.com>
2023-06-20 10:30:40 +0200

    wip

--- tests/python/qe-core/openbroadcastersoftware.py
+++ tests/python/qe-core/openbroadcastersoftware.py
@@ -5,16 +5,17 @@ def run(self):
     perl.require("serial_terminal")
     perl.require("utils")
     perl.require("x11utils")
-    #ensure_installed("flatpak")
+
+
     perl.serial_terminal.select_serial_terminal()
     perl.utils.zypper_call("in flatpak")
-
     assert_script_run("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
     assert_script_run("flatpak install -y com.obsproject.Studio",300)
-    #assert_script_run("flatpak install -y com.obsproject.Studio", timeout=300) DOES NOT WORK
 
     select_console('x11')
+    perl.x11utils.turn_off_gnome_screensaver()
     perl.x11utils.x11_start_program("flatpak run com.obsproject.Studio", "target_match", "flatpak-obsproject")
+
     # sometimes send_key "alt-f4" doesn't work reliable, so repeat it and exit
     send_key_until_needlematch("generic-desktop", "alt-f4", 6, 5)
 
