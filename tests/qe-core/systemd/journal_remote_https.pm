8d39959ff8c9badfb4c124bc688a5150fb643856
Michael Grifalconi <mgrifalconi@suse.com>
2024-10-21 15:15:44 +0200

    wip

--- tests/qe-core/systemd/journal_remote_https.pm
+++ tests/qe-core/systemd/journal_remote_https.pm
@@ -79,7 +79,7 @@ sub run {
     assert_script_run("echo 'URL=https://127.0.0.1:9090' >> $journal_upload_conf");
     assert_script_run("echo 'ServerKeyFile=/etc/journal/private/journal-upload-key.pem' >> $journal_upload_conf");
     assert_script_run("echo 'ServerCertificateFile=/etc/journal/certs/journal-upload-cert.pem' >> $journal_upload_conf");
-    assert_script_run("echo 'TrustedCertificateFile=/etc/journal/ca/journal-remote-ca-cert.pem' >> $journal_upload_conf");
+    #assert_script_run("echo 'TrustedCertificateFile=/etc/journal/ca/journal-remote-ca-cert.pem' >> $journal_upload_conf"); # TODO: cannot use together with --trust=all
 
 
 
