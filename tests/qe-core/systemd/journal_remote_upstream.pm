2edfb2f5589a3f37c0c36a639422fa3c913dc8db
Michael Grifalconi <mgrifalconi@suse.com>
2024-10-22 11:48:54 +0200

    wip

--- tests/qe-core/systemd/journal_remote_upstream.pm
+++ tests/qe-core/systemd/journal_remote_upstream.pm
@@ -17,6 +17,7 @@ use utils;
 sub run {
 
     select_serial_terminal;
+    zypper_call("in systemd-journal-remote");
     assert_script_run 'wget --quiet ' . data_url('console/journal_remote_upstream.sh');
     assert_script_run 'chmod +x journal_remote_upstream.sh';
     assert_script_run "./journal_remote_upstream.sh", 300;
